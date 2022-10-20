8c9d0b182a618c7507e345090762859e6d6ffe9a
Irinaka Robdone <irinaka.robodone@gmail.com>
2022-10-20 20:08:05 +0900

    updated mine.py, readme.md and try_json.py

--- mine.py
+++ mine.py
@@ -1,11 +1,19 @@
-from tkinter import*
+import json
 from tkinter import ttk
+from tkinter import*
+
+def get_jp(x):
+    Japanese =data["students"][int(x)]["japanese"]
+    w_label_sc["text"] = Japanese
+
+f = open("student_score.json")
+data = json.load(f)
 
 root=Tk()
 
 w_label=ttk.Label(text="生徒番号")
 w_entry=ttk.Entry(width=5)
-w_button=ttk.Button(text="テスト結果表示")
+w_button=ttk.Button(text="テスト結果表示",command = lambda:get_jp(w_entry.get()))
 w_label_jp=ttk.Label(text="日本語の得点")
 w_label_sc=ttk.Label(text="0")
 
@@ -16,3 +24,5 @@ w_label_jp.grid(row=2,column=0)
 w_label_sc.grid(row=3,column=1)
 
 root.mainloop()
+
+f.close()
--- try_json.py
+++ try_json.py
@@ -1,8 +1,30 @@
 import json
+math_1 = 0
+all = []
+zero = 0
+one = 0
+two = 0
+three = 0
+
 
 f=open("student_score.json")
 data=json.load(f)
-print("---------生徒一人目---------")
-print(data["students"][0])
+print("len of data",len(data))
+print("len of students",len(data["students"]))
+data_=data["students"]
+
+
+for i in range(len(data_)):
+    print("---------生徒",i+1,"人目---------")
+    print(data_["students"][i]["math"]["japanese"]["Science"]["Social"])
+
+
+# math_1 += data_[i]["math"]
+# math_1 /= 4
+print(math_1)
+
+avg = 50
+all.append(avg)
+print(all)
 
 f.close()
