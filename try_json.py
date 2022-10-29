4605301ce673df7b95a010a44bf3c85f8df40feb
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-10-29 11:41:29 +0900

    updated try_json.py by ji

--- try_json.py
+++ try_json.py
@@ -3,21 +3,23 @@ import json
 
 f = open("student_score.json")
 data = json.load(f)
+f.close()
 # print("-------生徒1人目-------")
 # p\rint(data["students"])
 
 
 scores = data["students"]
-# a = scores[0]["math"]
-# b = scores[0]["Science"]
-# c = scores[0]["japanese"]
-# d = scores[0]["Social"]
-# avg = a+b+c+d
-# avg /= 4
-avg =0
-for i in range(len(scores)):
-     avg += scores[i]["math"]
-avg/= len(scores)
 
-print(avg)
-f.close()
+avgs =[]
+# for i in range(len(scores)):
+#      avg += scores[i]["math"]
+# avg/= len(scores)
+for i in range(len(scores)):
+    a = scores[i]["math"]
+    b = scores[i]["Science"]
+    c = scores[i]["japanese"]
+    d = scores[i]["Social"]
+    avg = a+b+c+d
+    avg /= 4
+    avgs.append(avg)
+print(avgs)
