aefba67aa0ede1f557e6f51002c855b57aecdaad
Irinaka Robdone <irinaka.robodone@gmail.com>
2022-11-10 20:21:39 +0900

    init commit by ka

--- try_json.py
+++ try_json.py
@@ -1,30 +1,9 @@
-import json
-math_1 = 0
-all = []
-zero = 0
-one = 0
-two = 0
-three = 0
+import json 
 
-
-f=open("student_score.json")
-data=json.load(f)
-print("len of data",len(data))
-print("len of students",len(data["students"]))
-data_=data["students"]
-
-
-for i in range(len(data_)):
-    print("---------生徒",i+1,"人目---------")
-    print(data_["students"][i]["math"]["japanese"]["Science"]["Social"])
-
-
-# math_1 += data_[i]["math"]
-# math_1 /= 4
-print(math_1)
-
-avg = 50
-all.append(avg)
-print(all)
-
+f = open("student_score.json")
+data = json.load(f)
+print("-------生徒一人目-------")
+print(data["students"][3]["japanese"])
+print(data["students"][2]["japanese"])
+print(data["students"][1]["japanese"])
 f.close()
