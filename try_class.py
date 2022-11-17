8bea9d48a06afbc9c50da534daaa5df159046d34
Irinaka Robdone <irinaka.robodone@gmail.com>
2022-11-17 20:08:30 +0900

    updated try_json.py

---
+++ try_class.py
@@ -0,0 +1,19 @@
+
+class player():
+    
+    def __init__(self,hp,speed,kasikosa) -> None:
+        self.hp = hp
+        self.speed = speed
+        self.kasikosa = kasikosa
+        
+    def attack(self):
+        
+        player
+        
+    def die(self)
+    sel.hp = 0
+    print("!!Game Over!!")
+    
+    
+player = player()
+player.die()
--- try_json.py
+++ try_json.py
@@ -2,11 +2,26 @@ import json
 
 f = open("student_score.json")
 data = json.load(f)
+data = data["students"]
+goukei=0
+heikin=0
 
 #平均点　for文を使う
-for i in range(4)
+for i in range(4):
         pass
-合計/(個数)=aaa
+
 print("-------生徒一人目-------")
 
 f.close()
+
+
+
+for i in range(len(data)):
+        print(data[i]["math"])
+        print(data[i]["japanese"])
+        goukei+=data[i]["math"]
+        goukei+=data[i]["japanese"]
+        print(data[i]["math"])
+        print(data[i]["japanese"])
+#heikin=goukei/len(data)
+print(goukei)
